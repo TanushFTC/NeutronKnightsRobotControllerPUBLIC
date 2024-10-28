ce3e0ab4a67fc44947e46ed87d0a0a4f47fb6b53
= <=>
2024-10-28 19:16:44 -0400

    Lost a ton of work and redid Drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/mechanisms/inputs/gamepad/buttonPressedEvent/ButtonPressedEventListener.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms.inputs.gamepad.buttonPressedEvent;
-
-public interface ButtonPressedEventListener {
-    public void handleButtonPressed(ButtonPressedEvent buttonPressed);
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/opmodes/RobotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/opmodes/RobotTeleOp.java
@@ -4,42 +4,46 @@ import static org.firstinspires.ftc.neutronknightscode.main.robot.Robot.drivetra
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms.inputs.gamepad.ButtonPressedEventHandler;
-import org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms.inputs.gamepad.buttonPressedEvent.ButtonPressedEventSource;
-import org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms.inputs.gamepad.gamepadPresets.GamepadPresets;
-
 @TeleOp
 public class RobotTeleOp extends RobotOpMode {
-    private ButtonPressedEventSource gamepad;
-    private GamepadPresets preset;
-    private ButtonPressedEventHandler handler;
-    @Override
-    public void init() {
-        super.init();
-        gamepad = new ButtonPressedEventSource();
-        preset = GamepadPresets.DEFAULT;
-        handler = new ButtonPressedEventHandler(preset.getPreset());
-        gamepad.addButtonPressedEventListener(handler);
-    }
+
+    private boolean setInverted = false;
+    private boolean inverted = false;
+
     @Override
     public void loop() {
-        gamepad.checkButtonsPressed(gamepad1);
         double positivePower = gamepad1.right_stick_y-gamepad1.right_stick_x;
         double negativePower = gamepad1.right_stick_y+gamepad1.right_stick_x;
-        telemetry.addData("Gamepad Buttons Pressed", "Shows buttons pressed.");
-        telemetry.addLine(String.format("Slow Button: %b, Turning Mode Button: %b", handler.slow, handler.turningMode));
-        telemetry.update();
-        if(handler.slow){
-            positivePower = positivePower/2;
-            negativePower = negativePower/2;
+
+        double leftPower = gamepad1.left_trigger-gamepad1.right_trigger;
+        double rightPower = gamepad1.right_trigger-gamepad1.left_trigger;
+
+        double topLeftPower = positivePower+rightPower;
+        double bottomRightPower = positivePower+leftPower;
+        double topRightPower = negativePower+leftPower;
+        double bottomLeftPower = negativePower+rightPower;
+
+        if(gamepad1.left_bumper){
+            topLeftPower /= 2;
+            bottomRightPower /= 2;
+            topRightPower /=2;
+            bottomLeftPower /=2;
+        }
+        if(gamepad1.a && !setInverted){
+            setInverted = true;
+            inverted = !inverted;
+        }
+        if(inverted){
+            topLeftPower = -topLeftPower;
+            bottomRightPower = -bottomRightPower;
+            topRightPower = -topRightPower;
+            bottomLeftPower = -bottomLeftPower;
         }
         drivetrain.setPower(
-                positivePower,
-                -positivePower,
-                -negativePower,
-                negativePower
+                topLeftPower,
+                bottomRightPower,
+                topRightPower,
+                bottomLeftPower
         );
-        handler.slow = false;
-        handler.turningMode = false;
     }
 }
